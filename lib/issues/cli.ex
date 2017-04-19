adcd912c93322d7537fa21a784e345ed4755c5a6
Sean Handley <sean.handley@datacentred.co.uk>
2017-04-19 06:40:43 +0100

    More features and tests.

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -7,6 +7,7 @@
 # Visit http://www.pragmaticprogrammer.com/titles/elixir13 for more book information.
 #---
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
 
   @default_count 4
 
@@ -16,8 +17,10 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
 
-  def run(argv) do
-    parse_args(argv)
+  def main(argv) do
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -45,5 +48,32 @@ defmodule Issues.CLI do
     _ -> :help
 
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count}]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def decode_response({:ok, body}), do: body
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+    fn i1, i2 -> Map.get(i1, "created_at") <= Map.get(i2, "created_at") end
   end
 end
--- mix.exs
+++ mix.exs
@@ -12,7 +12,10 @@ defmodule Issues.Mixfile do
   def project do
     [
       app:             :issues,
+      escript:         escript_config(),
       version:         "0.0.1",
+      name:            "issues",
+      source_url:      "https://github.com/seanhandley/elixir-issues",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps:            deps()
@@ -22,7 +25,7 @@ defmodule Issues.Mixfile do
   # Configuration for the OTP application
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -35,6 +38,15 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~> 0.9"},
+      {:poison,    "~> 3.0"},
+      {:ex_doc,    "~> 0.12"},
+      {:earmark,   "~> 1.0", override: true}
+    ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
